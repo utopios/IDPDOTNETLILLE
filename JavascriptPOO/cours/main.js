cc3a15ec889ebaba020728e2fe68cb0bad5648ce
Administrateur <test@g.fr>
2022-04-19 11:20:41 +0200

    export import javascript

---
+++ JavascriptPOO/cours/main.js
@@ -0,0 +1,10 @@
+
+import {config1, direBonjour as Bonjour} from "./modules/configuration.js"
+
+import elementDefault from "./modules/configuration.js"
+
+//alert(config1)
+
+//Bonjour()
+
+alert(elementDefault)
---
+++ JavascriptPOO/cours/modules/configuration.js
@@ -0,0 +1,9 @@
+export const config1 = "Information configuration"
+
+export function direBonjour(){
+    alert("Bonjour")
+}
+
+const elementDefautModule = "je suis l'élément par défaut du module"
+
+export default elementDefautModule
