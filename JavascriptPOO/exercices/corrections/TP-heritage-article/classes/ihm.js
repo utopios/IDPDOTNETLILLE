eeb35cf880a77d3adfeb7254d395c24a896b5d17
Administrateur <test@g.fr>
2022-04-21 12:18:15 +0200

    correction promise

--- JavascriptPOO/exercices/corrections/TP-heritage-article/classes/ihm.js
+++ JavascriptPOO/exercices/corrections/TP-heritage-article/classes/ihm.js
@@ -17,6 +17,7 @@ export class Ihm {
     addEvent() {
         //Détection de l'event change sur la radio
         const radios = this.formulaire.querySelector("#parentRadio")
+        const loader = this.formulaire.querySelector("#loader")
         radios.addEventListener('change', (e) => {
             console.log(e.target.value)
             this.switchProduitService(e.target.value)
@@ -24,31 +25,52 @@ export class Ihm {
 
         this.formulaire.addEventListener('submit', (e) => {
             e.preventDefault()
-            this.ajouterArticle()
+            //Démarrer le loader
+            loader.style.display = 'block'
+            this.ajouterArticle().then(res => {
+                if (res.type == "produit") {
+                    this.domProduits.innerHTML += res.renduHtml
+                }
+                else if (res.type == "service") {
+                    this.domServices.innerHTML += res.renduHtml
+                }
+                loader.style.display = 'none'
+            }).catch(err => {
+                alert(err.message)
+                loader.style.display = 'none'
+            })
         })
     }
 
     ajouterArticle() {
-        const type = this.formulaire.querySelector("input[name='type']:checked").value
-        //Si produit => ajouterProduit
-        if(type == 'produit') {
-            this.ajouterProduit()
-        }
-        //Si Service => ajouterService
-        else if(type == 'service') {
-            this.ajouterService()
-        }
+        return new Promise((resolve, reject) => {
+            const type = this.formulaire.querySelector("input[name='type']:checked").value
+            setTimeout(() => {
+                //Si produit => ajouterProduit
+                if (type == 'produit') {
+                    resolve(this.ajouterProduit())
+                }
+                //Si Service => ajouterService
+                else if (type == 'service') {
+                    resolve(this.ajouterService())
+                }
+                else {
+                    reject(new { message: 'type incorrect' })
+                }
+            }, 3000)
+        })
     }
 
     ajouterProduit() {
         //Ajouter produit 
         //Récupération des champs
-        const {titre, description, prix, renduHtml} = this.constructionArticle()
-        const stock= document.querySelector("input[name='stock']").value
+        const { titre, description, prix, renduHtml } = this.constructionArticle()
+        const stock = document.querySelector("input[name='stock']").value
         //Faire des vérification sur les champs...
         const produit = new Produit(titre, prix, description, stock)
         this.articles.push(produit)
-        this.domProduits.innerHTML += `<tr>${renduHtml}<td>${stock}</td></tr>`
+        //this.domProduits.innerHTML += `<tr>${renduHtml}<td>${stock}</td></tr>`
+        return  { type: 'produit', renduHtml: `<tr>${renduHtml}<td>${stock}</td></tr>` }
     }
 
     ajouterService() {
@@ -63,35 +85,36 @@ export class Ihm {
         // this.domServices.innerHTML += `<tr><td>${titre}</td><td>${prix}</td><td>${description}</td><td>${domaine}</td></tr>`
 
         //<=> Avec un desctructeur 
-        const {titre, description, prix, renduHtml} = this.constructionArticle()
-        const domaine= document.querySelector("input[name='domaine']").value
+        const { titre, description, prix, renduHtml } = this.constructionArticle()
+        const domaine = document.querySelector("input[name='domaine']").value
         //Faire des vérification sur les champs...
         const service = new Service(titre, prix, description, domaine)
         this.articles.push(service)
-        this.domServices.innerHTML += `<tr>${renduHtml}<td>${domaine}</td></tr>`
+        //this.domServices.innerHTML += `<tr>${renduHtml}<td>${domaine}</td></tr>`
+        return  { type: 'service', renduHtml: `<tr>${renduHtml}<td>${domaine}</td></tr>` }
     }
 
     constructionArticle() {
         const titre = document.querySelector("input[name='titre']").value
-        const prix= document.querySelector("input[name='prix']").value
-        const description= document.querySelector("textarea[name='description']").value
+        const prix = document.querySelector("input[name='prix']").value
+        const description = document.querySelector("textarea[name='description']").value
         return {
-            titre: titre, 
-            prix: prix, 
-            description: description, 
-            renduHtml : `<tr><td>${titre}</td><td>${prix}</td><td>${description}</td>`
+            titre: titre,
+            prix: prix,
+            description: description,
+            renduHtml: `<tr><td>${titre}</td><td>${prix}</td><td>${description}</td>`
         }
     }
 
     switchProduitService(type) {
         const inputStock = this.formulaire.querySelector("input[name='stock']")
         const inputDomaine = this.formulaire.querySelector("input[name='domaine']")
-        if(type == "produit") {
-            
+        if (type == "produit") {
+
             inputDomaine.style.display = 'none'
             inputStock.style.display = 'block'
         }
-        else if(type =="service") {
+        else if (type == "service") {
             inputDomaine.style.display = 'block'
             inputStock.style.display = 'none'
         }
