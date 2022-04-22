940e1838f99075d98ad0b492a82cb0084c992b07
Administrateur <test@g.fr>
2022-04-22 11:20:25 +0200

    async await

--- JavascriptPOO/cours-promise/main.js
+++ JavascriptPOO/cours-promise/main.js
@@ -1,23 +1,39 @@
-const p = new Promise((resolve, reject) => {
-    //Logique métier dans la promise
-    let test = false
-    setTimeout(() => {
-        if(test) {
-            resolve({data : "les données envoyées par la promise"})
-        }
-        else {
-            reject({message: "Message d'erreur en cas d'echec de la promise"})
-        }
-    },5000)
+// const p = new Promise((resolve, reject) => {
+//     //Logique métier dans la promise
+//     let test = false
+//     setTimeout(() => {
+//         if(test) {
+//             resolve({data : "les données envoyées par la promise"})
+//         }
+//         else {
+//             reject({message: "Message d'erreur en cas d'echec de la promise"})
+//         }
+//     },5000)
+// })
+
+// //Flux d'execution
+
+// p.then((response) => {
+//     result.innerHTML = response.data
+// }).catch(err => {
+//     result.innerHTML = err.message
+// })
+
+// const result = document.querySelector("#result")
+// result.innerHTML = "En cours d'execution"
+
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() =>{
+        resolve("coucou")
+    },2000)
 })
 
-//Flux d'execution
+const fonctionAvecPromise = async () => {
+    // promise.then((res) => {
+    //     return res
+    // })
+    const result = await promise
+    return result
+}
 
-p.then((response) => {
-    result.innerHTML = response.data
-}).catch(err => {
-    result.innerHTML = err.message
-})
-
-const result = document.querySelector("#result")
-result.innerHTML = "En cours d'execution"
+console.log(fonctionAvecPromise())
