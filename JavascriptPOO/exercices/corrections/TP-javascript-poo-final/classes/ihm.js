4ae5ce9655d80df121c592cdf273f874018cbc47
Administrateur <test@g.fr>
2022-04-22 09:40:56 +0200

    debut de correction tp final

---
+++ JavascriptPOO/exercices/corrections/TP-javascript-poo-final/classes/ihm.js
@@ -0,0 +1,58 @@
+import { Todo } from "./todo"
+
+export class Ihm {
+    constructor(formulaireRecherche, formulaireAjout, resultat) {
+        this.todos = []
+        this.compteur = 0
+        this.formulaireAjout = formulaireAjout
+        this.formulaireRecherche = formulaireRecherche
+        this.resultat = resultat
+    }
+
+    demarrer() {
+
+    }
+
+    ajouterEvent() {
+        this.formulaireAjout.addEventListener('submit', (e) => {
+            e.preventDefault()
+            this.ajouterTodo().then(res => {
+                this.resultat.innerHTML += res.renduHTML
+            })
+        })
+    }
+
+    afficherToDos() {
+        
+    }
+
+    ajouterTodo() {
+        return new Promise((resolve, reject) => {
+            const titre = this.formulaireAjout.querySelector("input[name='titre']").value
+            const contenu = this.formulaireAjout.querySelector("textarea[name='contenu']").value
+            const todo = new Todo(++this.compteur, titre, contenu)
+            this.todos.push(todo)
+            resolve({renduHTML: `<tr><td>${todo.id}</td><td>${todo.titre}</td><td>${todo.contenu}</td></tr>`})
+        })
+    }
+
+    rechercherTodo() {
+        return new Promise((resolve, reject) => {
+
+        })
+    }
+
+    changerStatusTodo() {
+        return new Promise((resolve, reject) => {
+
+        })
+    }
+
+    supprimerTodo() {
+        return new Promise((resolve, reject) => {
+            
+        })
+    }
+
+
+}
---
+++ JavascriptPOO/exercices/corrections/TP-javascript-poo-final/classes/todo.js
@@ -0,0 +1,8 @@
+export class Todo {
+    constructor(id, titre, contenu) {
+        this.id = id
+        this.titre = titre
+        this.contenu = contenu
+        this.status = false
+    }
+}
