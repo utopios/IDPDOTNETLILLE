28f015e00bd7979a10a198e96dfeb6486877343a
Administrateur <test@g.fr>
2022-04-25 10:52:34 +0200

    exercice 1

--- nodejs/cours/cours-express/index.js
+++ nodejs/cours/cours-express/index.js
@@ -2,16 +2,23 @@ import express from "express"
 
 const app = express()
 
+const data = []
+
+
+app.use(express.json())
+
 app.get('', (req, res) => {
     res.end("<h1>Hello from our first nodejs app</h1>")
     //res.json(req.headers)
 })
 
-app.get('/clients', (req, res) => {
-    res.json([
-        {nom : 'toto', prenom: 'tata'},
-        {nom : 'minet', prenom: 'titi'},
-    ])
+app.get('/data', (req, res) => {
+    res.json(data)
+})
+
+app.post('', (req, res) => {
+    data.push(req.body)
+    res.json(req.body)
 })
 //pour démarrer notre application web sur un port donné.
 app.listen(3000, () => {
