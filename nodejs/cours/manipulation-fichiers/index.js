cbaad83a56fc88a0fcf06913445adc6e486f073b
Administrateur <test@g.fr>
2022-04-26 10:13:20 +0200

    ecriture lecture fichier

---
+++ nodejs/cours/manipulation-fichiers/index.js
@@ -0,0 +1,29 @@
+import fs from "fs"
+
+//La lecture du contenu d'un fichier async
+// fs.readFile("fichier1.txt", (err, data) => {
+//     if(err == null)
+//         console.log(data.toString())
+//     else 
+//         console.log(err)
+// })
+
+//La lecture du contenu d'un fichier sync
+// const data = fs.readFileSync("fichier1.txt")
+// console.log(data.toString())
+
+//Ecriture du contenu dans un fichier async
+// fs.writeFile('fichier2.txt', "contenu ajouté",(err) => {
+
+// })
+
+//Ecriture du contenu dans un fichier sync
+// fs.writeFileSync("fichier3.txt", "contenu fichier 3")
+
+//Ajouter du contenu dans un fichier
+fs.appendFile("fichier.txt", "contenu ajouté", (err) => {
+    console.log(err)
+})
+
+//Une version sync
+fs.appendFileSync("fichier.txt", "\ncontenu fichier")
