1770b598024996a12a54d0e0a4d361cb42fe550a
Administrateur <test@g.fr>
2022-04-26 09:21:14 +0200

    debut de correction todo

---
+++ nodejs/exercices/corrections/api-todos/classes/app.js
@@ -0,0 +1,45 @@
+import { Todo } from "./todo"
+
+export class App {
+    constructor() {
+        this.todos = []
+        this.count = 0
+    }
+
+    //Méthode de création
+    createTodo(title, content) {
+        const todo = new Todo(++this.count, title, content)
+        this.todos.push(todo)
+    }
+
+    //Méthode pour récupérer un todo par id
+    findTodoById(id) {
+        return this.todos.find(t => t.id == id)
+    }
+
+    //Methode pour rechercher todos par titre
+    searchTodosByTitle(search) {
+        return this.todos.filter(t => t.title.includes(search))
+    }
+
+
+    //Methode pour mettre à jour le statut d'un todo
+    updateTodoStatus(id) {
+        const todo = this.findTodoById(id)
+        if(todo != undefined) {
+            todo.status = !todo.status
+            return true
+        }
+        return false
+    }
+
+    //Methode pour supprimer un todo
+    deleteTodo(id) {
+        const todo = this.findTodoById(id)
+        if(todo != undefined) {
+            this.todos = this.filter(t => t.id != id)
+            return true
+        }
+        return false
+    }
+}
---
+++ nodejs/exercices/corrections/api-todos/classes/todo.js
@@ -0,0 +1,8 @@
+export class Todo {
+    constructor(id, title, content) {
+        this.id = id
+        this.title = title,
+        this.content = content,
+        this.status = false
+    }
+}
