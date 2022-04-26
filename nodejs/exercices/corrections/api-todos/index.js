69396802b8166c475ef075ca69d9976e4d68ae32
Administrateur <test@g.fr>
2022-04-26 09:33:31 +0200

    suite correction

---
+++ nodejs/exercices/corrections/api-todos/index.js
@@ -0,0 +1,38 @@
+import express from "express"
+import { App } from "./classes/app.js"
+
+//Un objet pour gérer les données
+const dataService = new App()
+
+//Notre objet express
+const api = express()
+
+//Utilisation d'un middleware json
+api.use(express.json())
+
+//Endpoint pour créer un todo
+api.post('/todos', (req, res) => {
+    const {title, content} = req.body
+    if(title != undefined && content != undefined) {
+        dataService.createTodo(title, content)
+        res.json({message: "todo ajouté"})
+    }
+    res.json({message: "Merci de envoyer un titre et un contenu"})
+})
+
+//Endpoint pour récuperer la liste des todos
+api.get('/todos', (req, res) => {
+    res.json(dataService.todos)
+})
+
+//Endpoint pour récupérer un seul todo
+api.get('/todos/:id', (req, res) => {
+    const todo = dataService.findTodoById(req.params.id)
+    if(todo != undefined)
+        res.json(todo)
+    res.json({mesage: "aucun todo avec cet id"})
+})
+
+api.listen(80, () => {
+    
+})
