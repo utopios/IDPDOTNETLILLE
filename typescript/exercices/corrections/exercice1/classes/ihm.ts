545fb1a4f7d519cdc42de170a753e372d795b285
Administrateur <test@g.fr>
2022-04-27 16:29:48 +0200

    suite correction

---
+++ typescript/exercices/corrections/exercice1/classes/ihm.ts
@@ -0,0 +1,45 @@
+import { Formateur } from './formateur';
+import { Personne } from './personne';
+import { Stagiaire } from './stagiaire';
+import { poserUneQuestion } from './../tools/tools';
+
+
+export class Ihm {
+    personnes: Array<Personne> = []
+    compteur:number
+    constructor() {
+        this.compteur = 0
+    }
+
+    async menu() {
+        let choix: string = ""
+        do {
+            console.log("1-- ajouter un stagiaire")
+            console.log("2-- ajouter un formateur")
+            console.log("3-- afficher les personnes");
+            console.log();
+            console.log("0-- quitter");
+
+            choix = await poserUneQuestion("Merci de choisir : ")
+            switch (choix) {
+                case "1":
+                    const nomStagiaire = await poserUneQuestion("Merci de saisir le nom : ")
+                    const prenomStagiaire = await poserUneQuestion("Merci de saisir le prénom : ")
+                    const promotion = await poserUneQuestion("Merci de saisir la promotion : ")
+                    const stagiaire:Stagiaire = new Stagiaire(++this.compteur, nomStagiaire, prenomStagiaire, promotion)
+                    this.personnes.push(<Personne>stagiaire)
+                    break;
+                case "2":
+                    const nomFormateur = await poserUneQuestion("Merci de saisir le nom : ")
+                    const prenomFormateur = await poserUneQuestion("Merci de saisir le prénom : ")
+
+                    break;
+                case "3":
+                    this.personnes.forEach(p => {
+                        p.afficher()
+                    })
+                    break;
+            }
+        } while (choix != '0')
+    }
+}
