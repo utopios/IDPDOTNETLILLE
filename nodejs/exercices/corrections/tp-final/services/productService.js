fba2ac8ed7ca2e1cc0c07719def2cf4e80c9d944
Administrateur <test@g.fr>
2022-04-27 11:09:44 +0200

    ajout héritage

--- nodejs/exercices/corrections/tp-final/services/productService.js
+++ nodejs/exercices/corrections/tp-final/services/productService.js
@@ -1,30 +1,19 @@
 import {readFileSync, writeFileSync} from "fs"
 import { Product } from "../classes/product.js"
-export class ProductService {
+import { BaseService } from "./baseService.js"
+export class ProductService extends BaseService {
     constructor() {
-        this.count = 0
-        this.products = []
-        this.file = "data/produits.json"
+        super("data/produits.json")    
     }
-
-    readFromJson() {
-        const data = readFileSync(this.file).toString()
-        this.products = JSON.parse(data)
-        this.count = this.products.length > 0 ? this.products[this.products.length-1].id : 0
-    }
-
-    writeToJson() {
-        writeFileSync(this.file, JSON.stringify(this.products))
-    }
-
+    
     addProduct(title, price, stock) {
         const product = new Product(++this.count, title, price, stock)
-        this.products.push(product)
+        this.data.push(product)
         this.writeToJson()
     }
 
     getProductById(id) {
-        return this.products.find(p => p.id == id)
+        return this.data.find(p => p.id == id)
     }
 
     updateProductStockById(id, deltaStock) {
