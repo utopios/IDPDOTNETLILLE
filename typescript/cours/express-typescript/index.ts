b4c6681fbf5fb13f3da33e56b97f3591e93b9ee8
Administrateur <test@g.fr>
2022-04-28 09:45:03 +0200

    suite cours typescript

--- typescript/cours/express-typescript/index.ts
+++ typescript/cours/express-typescript/index.ts
@@ -1,10 +1,17 @@
 import express, {Express, Request, Response} from "express"
 import { IndexRequest } from "./interface/indexRequest"
 import { IndexResponse } from "./interface/indexResponse"
+import { Personne } from "./interface/Personne"
 
 const app:Express = express()
 
 app.get('/:id', (req:Request<IndexRequest>, res:Response<IndexResponse>) => {
     const id = req.params.id
     res.json({message: "response"})
+})
+
+app.post('data', (req:Request<any, any, Personne>, res:Response<Personne>) => {
+    const p:Personne = req.body
+
+    res.json({nom:"", prenom:""})
 })
