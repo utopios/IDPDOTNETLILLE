1492fafc68780fa2e850b0f273f6d26fc963a257
Administrateur <test@g.fr>
2022-04-29 14:03:16 +0200

    cours react

--- reactjs/first-react-app/src/App.js
+++ reactjs/first-react-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Bonjour } from './Bonjour';
 
-function App() {
+export function App() {
   return (
     <div className="App">
       <h1>Bonjour tout le monde, cava </h1>
+      <Bonjour />
     </div>
   );
 }
 
-export default App;
+
---
+++ reactjs/first-react-app/src/Bonjour.js
@@ -0,0 +1,8 @@
+
+export function Bonjour() {
+    return(
+        <div>
+            <h1>Bonjour tout le monde</h1>
+        </div>
+    )
+}
