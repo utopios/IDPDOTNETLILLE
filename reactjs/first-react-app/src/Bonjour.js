81c03db36384aa7e011fbc579ca5dc20acf78087
Administrateur <test@g.fr>
2022-04-29 14:43:28 +0200

    creation de composant

--- reactjs/first-react-app/src/Bonjour.js
+++ reactjs/first-react-app/src/Bonjour.js
@@ -1,8 +1,34 @@
+//=> Avec une fonction
+// export function Bonjour() {
+//     return(
+//         <div>
+//             <h1>Bonjour tout le monde</h1>
+//         </div>
+//     )
+// }
 
-export function Bonjour() {
-    return(
-        <div>
-            <h1>Bonjour tout le monde</h1>
-        </div>
-    )
+import { Component, PureComponent } from "react";
+
+//Avec une lambda
+// export const Bonjour = () => {
+//     return(
+//         <div>
+//             <h1>Bonjour tout le monde</h1>
+//         </div>
+//     )
+// }
+
+// export class Bonjour extends PureComponent {
+export class Bonjour extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>Bonjour tout le monde</h1>
+            </div>
+        )
+    }
 }
