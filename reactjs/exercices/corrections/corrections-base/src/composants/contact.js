2f3f66060973d02319cc2ba68ca00fbcf8f81e90
Administrateur <test@g.fr>
2022-05-02 11:11:36 +0200

    debut state

--- reactjs/exercices/corrections/corrections-base/src/composants/contact.js
+++ reactjs/exercices/corrections/corrections-base/src/composants/contact.js
@@ -1,15 +1,24 @@
-import {Component} from "react"
-export class Contact extends Component {
-    constructor(props) {
-        super(props)
-    }
+// import {Component} from "react"
+// export class Contact extends Component {
+//     constructor(props) {
+//         super(props)
+//     }
 
-    render() {
-        const {nom, prenom, telephone, email} = this.props.contact
-        return(
-            <div>
-                Nom : {nom}, Prénom : {prenom}, Téléphone : {telephone}, email: {email}
-            </div>
-        )
-    }
+//     render() {
+//         const {nom, prenom, telephone, email} = this.props.contact
+//         return(
+//             <div>
+//                 Nom : {nom}, Prénom : {prenom}, Téléphone : {telephone}, email: {email}
+//             </div>
+//         )
+//     }
+// }
+
+export const Contact = (props) => {
+    const { nom, prenom, telephone, email } = props.contact
+    return (
+        <div>
+            Nom : {nom}, Prénom : {prenom}, Téléphone : {telephone}, email: {email}
+        </div>
+    )
 }
--- reactjs/first-react-app/src/App.js
+++ reactjs/first-react-app/src/App.js
@@ -1,32 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 import { Bonjour } from './Bonjour';
+import {Component} from "react"
+export class App extends Component {
 
-const data = [
-  {
-    nom: "toto", prenom:"tata"
-  },
-  {
-    nom:"titi", prenom: "minet"
+  constructor(props) {
+    super(props)
+    this.state = {
+      data: [
+        {
+          nom: "toto", prenom: "tata"
+        },
+        {
+          nom: "titi", prenom: "minet"
+        }
+      ]
+    }
   }
-]
-
-export function App() {
-  return (
-    <div className="App">
-      <h1>Bonjour tout le monde, cava </h1>
-      {/* <Bonjour nom="toto" prenom="titi" />
+  render() {
+    return (
+      <div className="App">
+        <h1>Bonjour tout le monde, cava </h1>
+        {/* <Bonjour nom="toto" prenom="titi" />
       <Bonjour nom="tata" prenom="minet" />
       <Bonjour nom="titi" prenom="toto" /> */}
-      {
-        data.map((element, index) => {
-          return(
-            <Bonjour nom={element.nom} key={index} prenom={element.prenom} />
-          )
-        })
-      }
-    </div>
-  );
+        {
+          this.state.data.map((element, index) => {
+            return (
+              <Bonjour nom={element.nom} key={index} prenom={element.prenom} />
+            )
+          })
+        }
+      </div>
+    )
+  }
 }
+
 
 
