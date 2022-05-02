59ec489db1dbacaeb73bc4adc0d504568f244886
Administrateur <test@g.fr>
2022-05-02 11:33:12 +0200

    correction state contact

--- reactjs/exercices/corrections/corrections-base/src/composants/list-contacts.js
+++ reactjs/exercices/corrections/corrections-base/src/composants/list-contacts.js
@@ -1,18 +1,25 @@
 import {Component} from "react"
 import { Contact } from "./contact";
 export class ListContacts extends Component {
-    contacts = [
-        {nom : "nom contact 1", prenom: "prenom contact 1", telephone:"0101010101", email:"contact1@fr"},
-        {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
-        {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
-    ]
+    // contacts = [
+    //     {nom : "nom contact 1", prenom: "prenom contact 1", telephone:"0101010101", email:"contact1@fr"},
+    //     {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
+    //     {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
+    // ]
     constructor(props) {
         super(props);
+        this.state = {
+            contacts : [
+                {nom : "nom contact 1", prenom: "prenom contact 1", telephone:"0101010101", email:"contact1@fr"},
+                {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
+                {nom : "nom contact 2", prenom: "prenom contact 2", telephone:"0101010102", email:"contact2@fr"},
+            ]
+        }
     }
     render() { 
         return (  
             // this.contacts.map((c, i) => (<Contact key={i} nom={c.nom} prenom={c.prenom} telephone={c.telephone} email={c.email} />))
-            this.contacts.map((c, i) => (<Contact key={i} contact={c}/>))
+            this.state.contacts.map((c, i) => (<Contact key={i} contact={c}/>))
         );
     }
 }
