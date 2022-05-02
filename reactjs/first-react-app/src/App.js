bf90e491639346278bfaf06171d08fbcdfdf2fd3
Administrateur <test@g.fr>
2022-05-02 11:47:23 +0200

    utilistion du setState

--- reactjs/first-react-app/src/App.js
+++ reactjs/first-react-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Bonjour } from './Bonjour';
 import {Component} from "react"
 export class App extends Component {
-
+  
   constructor(props) {
     super(props)
     this.state = {
@@ -14,12 +14,22 @@ export class App extends Component {
         {
           nom: "titi", prenom: "minet"
         }
-      ]
+      ],
+      count : 0
     }
+  }
+  clickButton = (e) => {
+   this.setState({
+    data : [...this.state.data, {nom:"bb", prenom:"tt"}],
+    count: this.state.count + 1
+    })
+    
   }
   render() {
     return (
       <div className="App">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.clickButton}>valider</button>
         <h1>Bonjour tout le monde, cava </h1>
         {/* <Bonjour nom="toto" prenom="titi" />
       <Bonjour nom="tata" prenom="minet" />
