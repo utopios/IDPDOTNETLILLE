3ae3d405d1eeddba3b849f7ea164e520b725431e
Administrateur <test@g.fr>
2022-05-05 10:05:58 +0200

    panier

--- reactjs/exercices/corrections/app-product-router/src/components/cart.js
+++ reactjs/exercices/corrections/app-product-router/src/components/cart.js
@@ -1,15 +1,16 @@
 import { ProductButton } from "./product-button"
 
-export const Cart = (props) =>{
-    let total = 0
-    props.cart.map(p => total += p.price)
-    return(
+export const Cart = (props) => {
+
+    let total = props.cart.map(p => p.price).reduce((p1, p2) => p1 + p2 ,0)
+
+    return (
         <div>
             <h1>Panier</h1>
-            {props.cart.map((p, i) => 
-                (
-                    <div key={i}>{p.title}<ProductButton  id={p.id} removeFromCart={props.removeFromCart} addToCart={props.addToCart} isInCart={props.isInCart}></ProductButton></div>
-                )
+            {props.cart.map((p, i) =>
+            (
+                <div key={i}>{p.title}<ProductButton id={p.id} removeFromCart={props.removeFromCart} addToCart={props.addToCart} isInCart={props.isInCart}></ProductButton></div>
+            )
             )}
             <div>{total}€</div>
         </div>
