483db30c3916749fddbdd087f701d74043a8b775
Administrateur <test@g.fr>
2022-05-30 12:23:03 +0200

    variable d'env

--- IMC-REACT/calcul-imc-react/src/services/ApiService.js
+++ IMC-REACT/calcul-imc-react/src/services/ApiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const url = "http://"+process.env.REACT_APP_API+":3001/";
-
+//docker run -d -p 80:80 -e REACT_APP_API=172.17.0.2 react-with-stages
 export const get = path => {
   return axios.get(url + path);
 };
