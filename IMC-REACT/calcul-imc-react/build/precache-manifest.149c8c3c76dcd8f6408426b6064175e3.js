1456e36381abfdb52fc9d85cd659c3e608b64d61
Administrateur <test@g.fr>
2022-05-30 11:01:21 +0200

    avec build

---
+++ IMC-REACT/calcul-imc-react/build/precache-manifest.149c8c3c76dcd8f6408426b6064175e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212d696ea293651d5747d64833335f2c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b28ab36e5a1201befc9",
+    "url": "/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "df62ed7f9c78634d52ff",
+    "url": "/static/css/main.a0ac3964.chunk.css"
+  },
+  {
+    "revision": "5b28ab36e5a1201befc9",
+    "url": "/static/js/2.c0bf4ef3.chunk.js"
+  },
+  {
+    "revision": "0e7fc92b65c827d6f3ec70eaab58f3e6",
+    "url": "/static/js/2.c0bf4ef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df62ed7f9c78634d52ff",
+    "url": "/static/js/main.da4cfccd.chunk.js"
+  },
+  {
+    "revision": "e1b19a913e2266705bb2",
+    "url": "/static/js/runtime-main.b492722a.js"
+  }
+]);
