453f678ef1c0e9bc0a6ba78d678b613eeb98216b
Administrateur <test@g.fr>
2022-06-01 10:20:18 +0200

    support cours

--- FormationDotNet/CoursCSharpPartie1/Program.cs
+++ FormationDotNet/CoursCSharpPartie1/Program.cs
@@ -16,11 +16,26 @@ namespace CoursCSharpPartie1
             Console.WriteLine(nomComplet);
             Console.WriteLine(age);
             int @int = 10;*/
-            int a, b;
-            a = 10;
-            b = 20;
-            Console.WriteLine(a + b);
+            //int a, b;
+            //a = 10;
+            //b = 20;
+            //Console.WriteLine(a + " " + b);
+            //decimal a = 10;
+            //double b = 10;
+            //Console.WriteLine(a + b);
+            //Convertion des variables.
+            //string a = "10,4";
+            //double b = 30 + Convert.ToDouble(a);
+            //Console.WriteLine(b);
 
+            //Utilisation de la console
+            //Ecrire dans la console
+            //Console.WriteLine("Bonjour tout le monde");
+            //Console.Write("Toto");
+            //Console.Write("tata");
+            //Lire dans la console
+            string saisi = Console.ReadLine();
+            Console.WriteLine(saisi);
         }
     }
 }
