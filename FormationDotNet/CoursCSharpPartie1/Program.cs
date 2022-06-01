2541920f7723bf9341f0872173e854b18b1c87e1
Administrateur <test@g.fr>
2022-06-01 09:37:53 +0200

    formation dotnet cours

---
+++ FormationDotNet/CoursCSharpPartie1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using System;
+namespace CoursCSharpPartie1
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
