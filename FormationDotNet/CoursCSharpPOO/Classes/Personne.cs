708c61d983f70697add0ecbd320da03823f4fccc
Administrateur <test@g.fr>
2022-06-07 09:54:36 +0200

    debut poo c#

---
+++ FormationDotNet/CoursCSharpPOO/Classes/Personne.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursCSharpPOO.Classes
+{
+    class Personne
+    {
+        public string nom;
+        public string prenom;
+    }
+}
---
+++ FormationDotNet/CoursCSharpPOO/Program.cs
@@ -0,0 +1,7 @@
+﻿using CoursCSharpPOO.Classes;
+
+//Création d'une personne à partir de la classe personne
+Personne p = new Personne();
+p.prenom = "ihab";
+p.nom = "abadi";
+Console.WriteLine($"{p.nom} {p.prenom}");
