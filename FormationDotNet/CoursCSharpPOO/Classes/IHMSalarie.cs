8f27a7de824ac7a7560f19f98221ab5241ca29d3
Administrateur <test@g.fr>
2022-06-07 14:12:03 +0200

    correction tp salarié

---
+++ FormationDotNet/CoursCSharpPOO/Classes/IHMSalarie.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursCSharpPOO.Classes
+{
+    class IHMSalarie
+    {
+        private int nbSalaries;
+        private Salarie[] salaries;
+
+        public void Demarrer()
+        {
+            Console.Write("Merci de saisir le nombre de salarié : ");
+            nbSalaries = Convert.ToInt32(Console.ReadLine());
+            salaries = new Salarie[nbSalaries];
+            SaisirLesSalaries();
+            AfficherSalarie();
+            Console.WriteLine("Total des salaires : " + Salarie.TotalSalaire);
+            Console.WriteLine("Total des salaries : " + Salarie.Compteur);
+        }
+
+        private void AfficherSalarie()
+        {
+            foreach(Salarie s in salaries)
+            {
+                s.AfficherSalaire();
+            }
+        }
+
+        private void SaisirLesSalaries()
+        {
+            for(int i=0; i < nbSalaries; i++)
+            {
+                Console.Write("Merci de saisir le nom complet : ");
+                string nom = Console.ReadLine();
+                Console.Write("Merci de saisir le matricule : ");
+                string matricule = Console.ReadLine();
+                Console.Write("Merci de saisir le catégorie : ");
+                string categorie = Console.ReadLine();
+                Console.Write("Merci de saisir le service : ");
+                string service = Console.ReadLine();
+                Console.Write("Merci de saisir le salaire : ");
+                decimal salaire = Convert.ToDecimal(Console.ReadLine());
+                Salarie s = new Salarie(matricule, nom, categorie, service, salaire);
+                salaries[i] = s;
+            }
+        }
+    }
+}
--- FormationDotNet/CoursCSharpPOO/Program.cs
+++ FormationDotNet/CoursCSharpPOO/Program.cs
@@ -1,16 +1,18 @@
 ﻿using CoursCSharpPOO.Classes;
 
 //Création d'une personne à partir de la classe personne
-Personne p = new Personne();
-//p.prenom = "ihab";
-//p.nom = "abadi";
-//p.SetNom("abadi");
-//p.SetPrenom("ihab");
-p.Nom = "abadi";
-//Personne p = new Personne("Abadi", "Ihab");
-//Console.WriteLine($"{p.nom} {p.prenom}");
-p.Afficher();
+//Personne p = new Personne();
+////p.prenom = "ihab";
+////p.nom = "abadi";
+////p.SetNom("abadi");
+////p.SetPrenom("ihab");
+//p.Nom = "abadi";
+////Personne p = new Personne("Abadi", "Ihab");
+////Console.WriteLine($"{p.nom} {p.prenom}");
+//p.Afficher();
 
-//Pour accéder à un element statique
-Personne.Nombre = 34;
-Console.WriteLine(Personne.Nombre);
+////Pour accéder à un element statique
+//Personne.Nombre = 34;
+//Console.WriteLine(Personne.Nombre);
+IHMSalarie ihm = new IHMSalarie();
+ihm.Demarrer();
