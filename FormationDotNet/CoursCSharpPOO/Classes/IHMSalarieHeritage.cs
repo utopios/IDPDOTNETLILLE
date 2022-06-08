09952a9bc29e254f5f108fe3e87c49ae4ab53923
Administrateur <test@g.fr>
2022-06-08 14:00:21 +0200

    suite correction

---
+++ FormationDotNet/CoursCSharpPOO/Classes/IHMSalarieHeritage.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursCSharpPOO.Classes
+{
+    class IHMSalarieHeritage
+    {
+        private Salarie[] salaries;
+        private int nbSalaries;
+        private string choix;
+
+        public void Demarrer()
+        {
+            Console.Write("Merci de saisir le nombre de salarié : ");
+            nbSalaries = Convert.ToInt32(Console.ReadLine());
+            salaries = new Salarie[nbSalaries];
+            Console.Clear();
+            do
+            {
+                Menu();
+                choix = Console.ReadLine();
+                Console.Clear();
+                switch(choix)
+                {
+                    case "1":
+                        ActionAjoutEmploye();
+                        break;
+                    case "2":
+                        ActionAfficherSalaire();
+                        break;
+                    case "3":
+                        ActionRechercherEmploye();
+                        break;
+                }
+            } while (choix != "0");
+        }
+
+        private void Menu()
+        {
+            Console.WriteLine("1 -- Ajouter un employé ");
+            Console.WriteLine("2 -- Afficher le salaire des employés");
+            Console.WriteLine("3 -- Rechercher un employé");
+            Console.WriteLine("0 -- Quitter");
+        }
+
+        private void MenuAjout()
+        {
+            Console.WriteLine("1 -- Ajouter un salarié ");
+            Console.WriteLine("2 -- Ajouter un commercial");
+            Console.WriteLine("0 -- Retour");
+        }
+
+        private void ActionAjoutEmploye()
+        {
+
+        }
+        private void ActionAfficherSalaire()
+        {
+
+        }
+
+        private void ActionRechercherEmploye()
+        {
+
+        }
+
+        private void Suivant()
+        {
+            Console.WriteLine("Une touche pour continuer ...");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
