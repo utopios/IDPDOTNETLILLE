61e5d4598353ca58b42c4be3b7c5605466397361
Administrateur <test@g.fr>
2022-06-13 16:43:39 +0200

    suite correction

--- FormationDotNet/CorrectionCaiseEnregistreuse/Classes/CashRegistry.cs
+++ FormationDotNet/CorrectionCaiseEnregistreuse/Classes/CashRegistry.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace CorrectionCaisseEnregistreuse.Classes
 {
-    internal class CashRegistry
+    class CashRegistry
     {
+        private List<Product> products;
+        private List<Order> orders;
+
+        public CashRegistry()
+        {
+            products = new List<Product>();
+            orders = new List<Order>();
+        }
+
+        public Product GetProductById(int id)
+        {
+            //A Coder
+            return null;
+        }
+
+        public Product AddProduct(Product product)
+        {
+            //A Coder
+            return product;
+        }
+
+        public Order AddOrder(Order order, Payment payment)
+        {
+            //A coder
+            return order;
+        }
     }
 }
