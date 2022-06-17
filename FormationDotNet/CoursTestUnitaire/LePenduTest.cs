e134694f8c96692791ef18301a5eca860a16eacd
Administrateur <test@g.fr>
2022-06-17 13:56:24 +0200

    debut correction test pendu

---
+++ FormationDotNet/CoursTestUnitaire/LePenduTest.cs
@@ -0,0 +1,16 @@
+﻿using CoursCSharpPOO.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoursTestUnitaire
+{
+    [TestClass]
+    public class LePenduTest
+    {
+        private LePendu pendu = new LePendu();
+
+    }
+}
