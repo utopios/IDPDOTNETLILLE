d34ff42b07e18b73efa6d5feaa9b8ba8a867df5f
Administrateur <test@g.fr>
2022-06-17 14:09:50 +0200

    debut correction test pendu

--- FormationDotNet/CoursTestUnitaire/LePenduTest.cs
+++ FormationDotNet/CoursTestUnitaire/LePenduTest.cs
@@ -11,6 +11,47 @@ namespace CoursTestUnitaire
     public class LePenduTest
     {
         private LePendu pendu = new LePendu();
+        private FakeGenerateur generateur = new FakeGenerateur();
+        [TestMethod]
+        public void TestCharTest_c_true()
+        {
+            //Act
+            pendu.GenererMasque(generateur);
+            bool result = pendu.TestChar('c');
+            Assert.IsTrue(result);
+        }
 
+       
+
+        [TestMethod]
+        public void TestCharTest_nbEssai()
+        {
+            //Act
+            pendu.GenererMasque(generateur);
+            pendu.TestChar('t');
+            pendu.TestChar('c');
+            Assert.AreEqual(9, pendu.NbEssai);
+        }
+
+        [TestMethod]
+        public void TestCharTest_t_false()
+        {
+            //Act
+            pendu.GenererMasque(generateur);
+            bool result = pendu.TestChar('t');
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void TestCharTest_masque()
+        {
+            //Act
+            pendu.GenererMasque(generateur);
+            pendu.TestChar('t');
+            pendu.TestChar('c');
+            Assert.AreEqual("c**c**", pendu.Masque);
+        }
+
+        
     }
 }
