ff4822eadc59e671e1744ba0ad3e5e511d82ede2
Administrateur <test@g.fr>
2022-06-17 09:35:04 +0200

    cours test unitaire

---
+++ FormationDotNet/CoursTestUnitaire/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace CoursTestUnitaire
+{
+    //Annotation
+    [TestClass]
+    public class UnitTest1
+    {
+        //Annotation
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Pour écrire un test, on utilise le pattern AAA
+            //A => Arrange => Préparer les objets à tester
+            //A => Act => Executer la méthode qu'on veut tester avec des paramètres spécifiques.
+            //A => Assert => Vérfier que le resultat de l'act est correct
+
+        }
+    }
+}
