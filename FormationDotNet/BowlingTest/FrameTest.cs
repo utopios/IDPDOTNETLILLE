22237c1a511d706bd6a8f070610c7f0ccc76b1e8
Administrateur <test@g.fr>
2022-06-20 09:48:59 +0200

    correction bowling

--- FormationDotNet/BowlingTest/FrameTest.cs
+++ FormationDotNet/BowlingTest/FrameTest.cs
@@ -57,5 +57,89 @@ namespace BowlingTest
             bool result = frame.Roll();
             Assert.IsFalse(result);
         }
+
+
+        [TestMethod]
+        public void Roll_LastFrame_SecondRoll_FirstRollStrick_ReturnTrue()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r = new Roll(10);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r };
+            bool result = frame.Roll();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_SecondRoll_FirstRollStrick_CheckScore()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r = new Roll(10);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r };
+            frame.Roll();
+            Assert.AreEqual(13, frame.Score);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_ThirdRoll_FirstRollStrick_ReturnTrue()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r1 = new Roll(10);
+            Roll r2 = new Roll(3);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r1, r2 };
+            bool result = frame.Roll();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_ThirdRoll_FirstRollStrick_CheckScore()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r1 = new Roll(10);
+            Roll r2 = new Roll(3);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r1, r2 };
+            frame.Roll();
+            Assert.AreEqual(16, frame.Score);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_ThirdRoll_Spare_ReturnTrue()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r1 = new Roll(7);
+            Roll r2 = new Roll(3);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r1, r2 };
+            bool result = frame.Roll();
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_ThirdRoll_Spare_CheckScore()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r1 = new Roll(7);
+            Roll r2 = new Roll(3);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r1, r2 };
+            frame.Roll();
+            Assert.AreEqual(16, frame.Score);
+        }
+
+        [TestMethod]
+        public void Roll_LastFrame_FourthRoll_ReturnFalse()
+        {
+            Mock.Get(_generator).Setup(m => m.RandomPins(10)).Returns(3);
+            Roll r1 = new Roll(7);
+            Roll r2 = new Roll(3);
+            Roll r3 = new Roll(10);
+            frame = new Frame(_generator, false);
+            frame.Rolls = new List<Roll>() { r1, r2, r3 };
+            bool result = frame.Roll();
+            Assert.IsFalse(result);
+        }
     }
 }
