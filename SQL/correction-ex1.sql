18d73d525c4a30088d30687afcee96dc44fe5c38
Administrateur <test@g.fr>
2022-06-21 09:19:22 +0200

    correction exercice 1

---
+++ SQL/correction-ex1.sql
@@ -0,0 +1,24 @@
+﻿--Correction exercice 1
+--Q1
+CREATE TABLE personne(
+id int not null PRIMARY KEY IDENTITY(1,1),
+titre varchar(10) not null,
+prenom varchar(200) not null,
+nom varchar(200) not null,
+email varchar(200) not null,
+telephone varchar(10) not null,
+);
+
+--Q2
+
+INSERT INTO personne (titre, nom, prenom, telephone, email)
+values 
+('Mr', 'aa', 'bb', '012', 'aa@bb.fr'),
+('Mme', 'bb', 'cc', '013', 'dd@cc.fr'),
+('Mr', 'ff', 'ee', '022', 'ff@ee.fr');
+
+--Q3
+SELECT * FROM personne order by nom asc;
+
+--Q4
+SELECT * FROM personne order by titre asc;
