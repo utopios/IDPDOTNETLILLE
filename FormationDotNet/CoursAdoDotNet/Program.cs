b09c6b9f57ba216813c80a5937c7e52f8e1d0a93
Administrateur <test@g.fr>
2022-06-21 16:09:27 +0200

    ado.net

--- FormationDotNet/CoursAdoDotNet/Program.cs
+++ FormationDotNet/CoursAdoDotNet/Program.cs
@@ -6,10 +6,20 @@ using Microsoft.Data.SqlClient;
 //Ado.net Connexion à une base de données sql server
 string connectionString = @"Data Source=(LocalDb)\cousDotNet;Integrated Security=True"; 
 SqlConnection connection = new SqlConnection(connectionString);
-
+string nom = "abadi", 
+    prenom = "ihab", 
+    email = "ihab@utopios.net", 
+    telephone = "01010101", 
+    titre = "Mr";
 string sql = "INSERT INTO personne (titre, nom, prenom, email, telephone)" +
-    " values ('Mr', 'sql', 'sql', 'sql', 'sql')";
+    " values (@titre, @nom, @prenom, @email, @telephone)";
 SqlCommand cmd = new SqlCommand(sql, connection);
+cmd.Parameters.Add(new SqlParameter("@titre", titre));
+cmd.Parameters.Add(new SqlParameter("@prenom", prenom));
+cmd.Parameters.Add(new SqlParameter("@nom", nom));
+cmd.Parameters.Add(new SqlParameter("@telephone", telephone));
+cmd.Parameters.Add(new SqlParameter("@email", email));
+
 //Se connecter
 connection.Open();
 
