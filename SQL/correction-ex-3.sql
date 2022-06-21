6667345c477c261d9a3db3fb8aaa64c615c00519
Administrateur <test@g.fr>
2022-06-21 14:24:14 +0200

    suite correction

--- SQL/correction-ex-3.sql
+++ SQL/correction-ex-3.sql
@@ -19,3 +19,20 @@ SELECT count(*) as nombre_commune, d.departement_nom from villes_france_free as 
 inner join departement as d on v.ville_departement=d.departement_code 
 group by d.departement_code, d.departement_nom 
 order by nombre_commune desc;
+
+--Question 6
+
+SELECT SUM(v.ville_surface) as surface, d.departement_nom from villes_france_free as v 
+inner join departement as d on v.ville_departement=d.departement_code 
+group by d.departement_code, d.departement_nom 
+order by surface desc;
+
+-- Question 7
+SELECT count(*) as nb_ville from villes_france_free as v 
+where v.ville_nom like 'SAINT%';
+
+--Question 8
+SELECT * FROM (
+SELECT count(*) as nombre, v.ville_nom 
+from villes_france_free as v group by v.ville_nom)
+as tmp_ville where nombre > 1 order by nombre desc;
