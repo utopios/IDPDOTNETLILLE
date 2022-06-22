01df0cfdc04db63cabafa29d24284ddd495ea78f
Administrateur <test@g.fr>
2022-06-22 09:22:08 +0200

    correction

--- FormationDotNet/CoursAdoDotNet/Program.cs
+++ FormationDotNet/CoursAdoDotNet/Program.cs
@@ -6,33 +6,75 @@ using Microsoft.Data.SqlClient;
 //Ado.net Connexion à une base de données sql server
 string connectionString = @"Data Source=(LocalDb)\cousDotNet;Integrated Security=True"; 
 SqlConnection connection = new SqlConnection(connectionString);
-string nom = "abadi", 
-    prenom = "ihab", 
-    email = "ihab@utopios.net", 
-    telephone = "01010101", 
-    titre = "Mr";
-string sql = "INSERT INTO personne (titre, nom, prenom, email, telephone)" +
-    " values (@titre, @nom, @prenom, @email, @telephone)";
-SqlCommand cmd = new SqlCommand(sql, connection);
-cmd.Parameters.Add(new SqlParameter("@titre", titre));
-cmd.Parameters.Add(new SqlParameter("@prenom", prenom));
-cmd.Parameters.Add(new SqlParameter("@nom", nom));
-cmd.Parameters.Add(new SqlParameter("@telephone", telephone));
-cmd.Parameters.Add(new SqlParameter("@email", email));
+//string nom = "abadi", 
+//    prenom = "ihab", 
+//    email = "ihab@utopios.net", 
+//    telephone = "01010101", 
+//    titre = "Mr";
+//string sql = "INSERT INTO personne (titre, nom, prenom, email, telephone)" +
+//    " values (@titre, @nom, @prenom, @email, @telephone)";
+//SqlCommand cmd = new SqlCommand(sql, connection);
+//cmd.Parameters.Add(new SqlParameter("@titre", titre));
+//cmd.Parameters.Add(new SqlParameter("@prenom", prenom));
+//cmd.Parameters.Add(new SqlParameter("@nom", nom));
+//cmd.Parameters.Add(new SqlParameter("@telephone", telephone));
+//cmd.Parameters.Add(new SqlParameter("@email", email));
 
-//Se connecter
-connection.Open();
+////Se connecter
+//connection.Open();
 
-//Executer la commande
-//1 - en utilisant la méthode nonQuery
-int nbLigne = cmd.ExecuteNonQuery();
+////Executer la commande
+////1 - en utilisant la méthode nonQuery
+//int nbLigne = cmd.ExecuteNonQuery();
 
-//Libération des ressources
-cmd.Dispose();
-//Fermer la connexion
-connection.Close();
+////Libération des ressources
+//cmd.Dispose();
+////Fermer la connexion
+//connection.Close();
 
-Console.WriteLine(nbLigne);
+//Console.WriteLine(nbLigne);
 
 //Ecrire un programme qui demande à l'utilisateur de saisir
 //titre produit, prix et stock et les ajoute dans une table produit
+
+//Correction
+
+string request = "CREATE TABLE produit (" +
+    "id int PRIMARY KEY IDENTITY(1,1)," +
+    "titre varchar(100) not null," +
+    "prix decimal not null," +
+    "stock int not null"+
+    ")";
+SqlCommand command = new SqlCommand(request, connection);
+connection.Open();
+try
+{
+    command.ExecuteNonQuery();
+    Console.WriteLine("table créée");
+}catch(Exception ex)
+{
+    Console.WriteLine("Table existe");
+}
+finally
+{
+    command.Dispose();
+}
+Console.Write("Merci de saisir le titre du produit : ");
+string titre = Console.ReadLine();
+Console.Write("Merci de saisir le prix du produit : ");
+decimal prix = Convert.ToDecimal(Console.ReadLine());
+Console.Write("Merci de saisir le stock du produit : ");
+int stock = Convert.ToInt32(Console.ReadLine());
+
+request = "INSERT INTO produit (titre, prix, stock) " +
+    "values (@titre, @prix, @stock)";
+
+command = new SqlCommand(request, connection);
+command.Parameters.Add(new SqlParameter("@titre", titre));
+command.Parameters.Add(new SqlParameter("@prix", prix));
+command.Parameters.Add(new SqlParameter("@stock", stock));
+
+int nbLigne = command.ExecuteNonQuery();
+Console.WriteLine(nbLigne);
+command.Dispose();
+connection.Close();
