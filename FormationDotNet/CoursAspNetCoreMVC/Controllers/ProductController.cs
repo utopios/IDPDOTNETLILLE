f191770915442bea4e1d84e9b5b5caf2d11119ac
Administrateur <test@g.fr>
2022-07-12 10:00:35 +0200

    debut cours asp.net core mvc

---
+++ FormationDotNet/CoursAspNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "Bonjour je suis la méthode index du controleur produit";
+        }
+    }
+}
