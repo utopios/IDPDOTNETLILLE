547d4859421abcbc3da4420dfb9260c045a74c45
Administrateur <test@g.fr>
2022-07-12 10:27:58 +0200

    debut cours asp.net core mvc

---
+++ FormationDotNet/CoursAspNetCoreMVC/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCoreMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public string Index()
+        {
+            return "Acccueil annuaire contact";
+        }
+
+        public string Form()
+        {
+            return "Page formulaire";
+        }
+        public string DetailContact()
+        {
+            return "Page détail contact";
+        }
+    }
+}
