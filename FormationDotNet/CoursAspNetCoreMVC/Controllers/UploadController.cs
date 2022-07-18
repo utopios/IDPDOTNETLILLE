43d8a7774d608a7b32ee46cf794821c451459a4b
Administrateur <test@g.fr>
2022-07-18 14:27:33 +0200

    cours upload

---
+++ FormationDotNet/CoursAspNetCoreMVC/Controllers/UploadController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoursAspNetCoreMVC.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+
+        private IWebHostEnvironment _env;
+
+        public UploadController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Upload/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Upload/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Upload/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Upload/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Upload/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Upload/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Upload/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public IActionResult SubmitForm(IFormFile image)
+        {
+            string path = Path.Combine(_env.WebRootPath,"images", image.FileName);
+            Stream stream = new FileStream(path, FileMode.Create);
+            image.CopyTo(stream);
+            stream.Close();
+            return RedirectToAction("Index");
+        }
+    }
+}
