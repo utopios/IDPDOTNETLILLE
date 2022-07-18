c1afe4613208b32d85881a82f32297144ec077f9
Administrateur <test@g.fr>
2022-07-18 10:23:04 +0200

    Injection de dépendance

---
+++ FormationDotNet/CorrectionCompteBancaireAspNet/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using BankEntityFrameWork.Classes;
+using BankEntityFrameWork.Repositories;
+using BankEntityFrameWork.Tools;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorrectionCompteBancaireAspNet.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SubmitCreateAccount(Account  account, Customer customer)
+        {
+            //AccountRepository accountRepository = new AccountRepository(new DataContext());
+            return RedirectToAction("Index");
+        }
+    }
+}
