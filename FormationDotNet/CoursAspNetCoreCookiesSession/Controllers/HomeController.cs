d274f25856223a1e6baac97e9a56ceedbe8dd930
Administrateur <test@g.fr>
2022-07-19 14:16:54 +0200

    cours cookies et session

---
+++ FormationDotNet/CoursAspNetCoreCookiesSession/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using CoursAspNetCoreCookiesSession.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoursAspNetCoreCookiesSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(1)
+            };
+            HttpContext.Response.Cookies.Append("key-cookie", "value of cookie", options);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            string value = HttpContext.Request.Cookies["key-cookie"];
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
