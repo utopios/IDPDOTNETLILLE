2a4c5c1f1c00c8348d5404c5f068445ef293d4dc
Administrateur <test@g.fr>
2022-07-21 14:43:00 +0200

    Extension de méthode

---
+++ FormationDotNet/CorrectionCaisseEnregistreuseAspNetCore/Tools/Extension.cs
@@ -0,0 +1,19 @@
+﻿using CashRegistryEntityFrameWork.Classes;
+using CashRegistryEntityFrameWork.Repositories;
+using CorrectionCaisseEnregistreuseAspNetCore.Interfaces;
+using CorrectionCaisseEnregistreuseAspNetCore.Services;
+
+namespace CorrectionCaisseEnregistreuseAspNetCore.Tools
+{
+    public static class Extension
+    {
+        public static void AddAllServices(this IServiceCollection Services)
+        {
+            Services.AddScoped<BaseRepository<Product>, ProductRepository>();
+            Services.AddScoped<BaseRepository<Order>, OrderRepository>();
+            Services.AddScoped<BaseRepository<CashRegistryUser>, CashRegistryUserRepository>();
+            Services.AddScoped<ICart, CartSessionService>();
+            Services.AddScoped<ILogin, LoginSessionService>();
+        }
+    }
+}
