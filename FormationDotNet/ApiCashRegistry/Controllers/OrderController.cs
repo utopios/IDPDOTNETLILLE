93e976d20309b847293ad59c3e1a7203cf280b44
Administrateur <test@g.fr>
2022-07-25 14:32:00 +0200

    api cash registry

---
+++ FormationDotNet/ApiCashRegistry/Controllers/OrderController.cs
@@ -0,0 +1,25 @@
+﻿using ApiCashRegistry.Services;
+using ApiCashRegistry.Tools;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCashRegistry.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private OrderService _orderService;
+
+        public OrderController(OrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        [HttpPost]
+        public IActionResult Post(OrderRequestDTO orderRequestDTO)
+        {
+            return Ok(_orderService.AddOrder(orderRequestDTO));
+        }
+    }
+}
---
+++ FormationDotNet/ApiCashRegistry/Services/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using ApiCashRegistry.Tools;
+using CashRegistryEntityFrameWork.Classes;
+using CashRegistryEntityFrameWork.Repositories;
+using CashRegistryEntityFrameWork.Tools;
+
+namespace ApiCashRegistry.Services
+{
+    public class OrderService
+    {
+        private BaseRepository<Order> _orderRepository;
+        private BaseRepository<Product> _productRepository;
+        private DataContext _dataContext;
+
+        public OrderService(BaseRepository<Order> orderRepository, BaseRepository<Product> productRepository, DataContext dataContext)
+        {
+            _orderRepository = orderRepository;
+            _productRepository = productRepository;
+            _dataContext = dataContext;
+        }
+
+        public OrderResponseDTO AddOrder(OrderRequestDTO orderRequestDTO)
+        {
+            Order order = new Order();
+            CardPayment payment = new CardPayment();            
+            order.Payment = payment;
+            payment.Payment = order.Payment;
+            foreach(int i in orderRequestDTO.ProductsId)
+            {
+                order.AddProduct(_productRepository.Find(p => p.Id == i));
+            }
+            _orderRepository.Create(order);
+            return new OrderResponseDTO(order.Id, order.Total);
+        } 
+    }
+}
