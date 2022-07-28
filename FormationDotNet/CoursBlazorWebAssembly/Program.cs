9c8d4027b037b8596422906f2fc031eda2d98b48
Administrateur <test@g.fr>
2022-07-28 15:10:35 +0200

    debut cours blazor

--- FormationDotNet/CoursBlazorWebAssembly/Program.cs
+++ FormationDotNet/CoursBlazorWebAssembly/Program.cs
@@ -7,6 +7,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddSingleton<DemoStateService>();
+builder.Services.AddSingleton<ContactService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
---
+++ FormationDotNet/CoursBlazorWebAssembly/Services/ContactService.cs
@@ -0,0 +1,14 @@
+﻿using CoursBlazorWebAssembly.Models;
+
+namespace CoursBlazorWebAssembly.Services
+{
+    public class ContactService
+    {
+        public List<Contact> Contacts { get; set; }
+
+        public ContactService()
+        {
+            Contacts = new List<Contact>();
+        }
+    }
+}
