eaa3a7734d0cd9ba05801ef3bd38017931349756
Administrateur <test@g.fr>
2022-07-28 10:55:52 +0200

    debut cours blazor

---
+++ FormationDotNet/CoursBlazorWebAssembly/Models/Contact.cs
@@ -0,0 +1,10 @@
+﻿namespace CoursBlazorWebAssembly.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public string Phone { get; set; }
+    }
+}
