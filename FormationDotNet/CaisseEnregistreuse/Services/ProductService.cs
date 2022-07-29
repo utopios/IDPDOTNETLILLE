53b6c8773ef833b10c53282bda0ad98c9cbec6cd
Administrateur <test@g.fr>
2022-07-29 11:08:13 +0200

    debut de correction blazor

--- FormationDotNet/CaisseEnregistreuse/Services/ProductService.cs
+++ FormationDotNet/CaisseEnregistreuse/Services/ProductService.cs
@@ -5,13 +5,17 @@ namespace CaisseEnregistreuse.Services;
 public class ProductService : IProductService
 {
     private List<Product> _products;
+    private List<Product> _searchProducts;
     public event Action ProductsChanged;
+    public event Action SearchChanged;
     public ProductService()
     {
+        _searchProducts = new List<Product>();
         _products = new List<Product>() { new Product() { Id =1, Title="product 1", Price=10, Stock=10} };
     }
 
     public List<Product> Products { get => _products; }
+    public List<Product> SearchProducts { get => _searchProducts; set => _searchProducts = value; }
 
     public bool AddProduct(Product product)
     {
@@ -24,6 +28,12 @@ public class ProductService : IProductService
     public Product GetProductById(int id)
     {
         return _products.Find(p => p.Id == id);
+    }
+
+    public void SearchProductTitle(string search)
+    {
+        _searchProducts = _products.Where(p => p.Title.Contains(search)).ToList();
+        SearchChanged?.Invoke();
     }
 }
 
