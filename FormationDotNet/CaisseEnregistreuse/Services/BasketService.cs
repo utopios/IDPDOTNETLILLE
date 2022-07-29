bb92a717b5fecd04ecf3c68f1cf473b947235033
Administrateur <test@g.fr>
2022-07-29 11:37:50 +0200

    debut de correction blazor

---
+++ FormationDotNet/CaisseEnregistreuse/Services/BasketService.cs
@@ -0,0 +1,32 @@
+﻿using CaisseEnregistreuse.Models;
+
+namespace CaisseEnregistreuse.Services
+{
+    public class BasketService
+    {
+        public List<Product> Products { get; set; }
+
+        public decimal Total { get
+            {
+                decimal total = 0;
+                Products.ForEach(p => total += p.Price);
+                return total;
+            } 
+        }
+
+        public BasketService()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            Products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            Products.Add(product);
+        }
+    }
+}
