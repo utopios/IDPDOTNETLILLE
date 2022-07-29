7c6e4aadf7f957bbe2ed205b78d1f0b165b2f333
Administrateur <test@g.fr>
2022-07-29 14:01:28 +0200

    debut de correction blazor

--- FormationDotNet/CycleVieComposant/Program.cs
+++ FormationDotNet/CycleVieComposant/Program.cs
@@ -1,6 +1,7 @@
 using CycleVieComposant;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using System.Net.Http.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
--- FormationDotNet/coursApiRest/Program.cs
+++ FormationDotNet/coursApiRest/Program.cs
@@ -3,13 +3,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("all", builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
-
+app.UseCors("all");
 app.MapControllers();
 
 app.Run();
