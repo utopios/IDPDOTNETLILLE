0a2e0e47c75b4ce1099d6536af1b4ff9175d7f64
Administrateur <test@g.fr>
2022-08-18 12:20:27 +0200

    lifecycle xamarin app

--- FormationDotNet/CoursXamarinForms/CoursXamarinForms/MainPage.xaml.cs
+++ FormationDotNet/CoursXamarinForms/CoursXamarinForms/MainPage.xaml.cs
@@ -3,9 +3,12 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using CoursXamarinForms.Classes;
+using Newtonsoft.Json;
 
 namespace CoursXamarinForms
 {
@@ -29,6 +32,22 @@ namespace CoursXamarinForms
         private async void Button_Clicked_1(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new ListViewPage());
+        }
+
+        protected async override void OnAppearing()
+         {
+            HttpClient httpClient = new HttpClient();
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync("http://10.0.2.2:5211/weatherforecast");
+                HttpContent httpContent = response.Content;
+                string content = await response.Content.ReadAsStringAsync();
+                List<WeatherForecast> liste = JsonConvert.DeserializeObject<List<WeatherForecast>>(content);
+            }
+            catch(Exception e)
+            {
+                
+            }
         }
     }
 }
