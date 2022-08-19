66c47b03f7f1f1d284e610456dc52eea014e4640
Administrateur <test@g.fr>
2022-08-19 09:23:14 +0200

    create container ioc

---
+++ FormationDotNet/CoursXamarinForms/CoursXamarinForms/Tools/IOCContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoursXamarinForms.Tools
+{
+    public class IOCContainer
+    {
+        private Dictionary<Type, Type> _types = new Dictionary<Type, Type>();
+
+        public void Register<T,V>(T interfaceType, V instanceType)
+        {
+            _types.Add(interfaceType.GetType(), instanceType.GetType());
+        }
+    }
+}
