2a1c5115a388fb1383b7d4369f6ce3d96405f66c
Administrateur <test@g.fr>
2022-08-19 09:37:08 +0200

    create container ioc

--- FormationDotNet/CoursXamarinForms/CoursXamarinForms/Tools/IOCContainer.cs
+++ FormationDotNet/CoursXamarinForms/CoursXamarinForms/Tools/IOCContainer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reflection;
 using System.Text;
 
 namespace CoursXamarinForms.Tools
@@ -11,6 +12,20 @@ namespace CoursXamarinForms.Tools
         public void Register<T,V>(T interfaceType, V instanceType)
         {
             _types.Add(interfaceType.GetType(), instanceType.GetType());
+        }
+
+        public T Resolve<T>()
+        {
+            Type type = _types[typeof(T)];
+            ConstructorInfo[] constructors = type.GetConstructors();
+            ParameterInfo[] parameterInfos = constructors[0].GetParameters();
+            List<object> parameters = new List<object>();
+            foreach(ParameterInfo parameterInfo in parameterInfos)
+            {
+                Type t = _types[parameterInfo.ParameterType];
+                parameters.Add((T)t.GetConstructor(new Type[0]).Invoke(new object[0]));
+            }
+            return (T)type.GetConstructor(new Type[0]).Invoke(parameters.ToArray());
         }
     }
 }
