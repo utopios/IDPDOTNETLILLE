4760850dcccfca2d0c7dd003cd480d13e8abc9f4
Administrateur <test@g.fr>
2022-08-23 10:02:50 +0200

    exemple onglet

---
+++ FormationDotNet/CoursXamarinOnglet/CoursXamarinOnglet/Views/ItemsPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CoursXamarinOnglet.Models;
+using CoursXamarinOnglet.ViewModels;
+using CoursXamarinOnglet.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoursXamarinOnglet.Views
+{
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel _viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = _viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            _viewModel.OnAppearing();
+        }
+    }
+}
---
+++ FormationDotNet/CoursXamarinOnglet/CoursXamarinOnglet/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using CoursXamarinOnglet.Models;
+using CoursXamarinOnglet.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CoursXamarinOnglet.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
