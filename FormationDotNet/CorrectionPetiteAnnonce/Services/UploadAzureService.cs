56890f81523ad476351f7d7afcb7f6c27efb9b13
Administrateur <test@g.fr>
2022-08-30 09:49:45 +0200

    avec azure identity

--- FormationDotNet/CorrectionPetiteAnnonce/Services/UploadAzureService.cs
+++ FormationDotNet/CorrectionPetiteAnnonce/Services/UploadAzureService.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
 using CorrectionPetiteAnnonce.Interfaces;
 
 namespace CorrectionPetiteAnnonce.Services
@@ -9,7 +10,8 @@ namespace CorrectionPetiteAnnonce.Services
 
         public UploadAzureService()
         {
-            blobContainerClient = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=utopios;AccountKey=eYFsdjjsLbUfqt4TE9m3EKBDbHmmo/IzRHqHAMGcY6K+qoGGoYJPWG5mnVRG54VhFwF5XEMl55h6+AStP+Olzg==;EndpointSuffix=core.windows.net", "images-ihab");
+            //blobContainerClient = new BlobContainerClient("DefaultEndpointsProtocol=https;AccountName=utopios;AccountKey=eYFsdjjsLbUfqt4TE9m3EKBDbHmmo/IzRHqHAMGcY6K+qoGGoYJPWG5mnVRG54VhFwF5XEMl55h6+AStP+Olzg==;EndpointSuffix=core.windows.net", "images-ihab");
+            blobContainerClient = new BlobContainerClient(new Uri("utopios.blob.core.windows.net/images-ihab"), new DefaultAzureCredential());
         }
         public string Upload(IFormFile file)
         {
